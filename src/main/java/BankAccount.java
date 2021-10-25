13d3905f8109a7186fac004829da3ef7f73d6a21
anaypant <35899820+anaypant@users.noreply.github.com>
2021-10-25 12:02:29 -0700

    c

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -3,4 +3,49 @@ public class BankAccount {
     private int balance;
     private int accountNumber;
     private String preferredName;
+    public BankAccount(String name, int accountNumber){
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+        this.preferredName = name;
+    }
+
+   public void withdraw(int x){
+        if (x > this.balance){
+            System.out.println("ERROR excf007: USER ATTEMPTED TO WITHDRAW $" + x + " FROM ACCOUNT#" + this.accountNumber + " WITH A BALANCE OF $" + this.balance);
+            System.out.println("Sorry, you cannot withdraw more than what you have.\n");
+        }
+        else{
+            this.balance -= x;
+            System.out.println("Successfully withdrawn $" + x);
+        }
+
+   }
+
+   public void deposit(int x){
+        this.balance += x;
+   }
+
+   public int getBalance(){
+        return this.balance;
+   }
+
+   public int getAccountNumber(){
+        return this.accountNumber;
+   }
+
+   public String getPreferredName(){
+        return this.preferredName;
+   }
+
+   public void setPreferredName(String s){
+        this.preferredName = s;
+   }
+
+   @Override
+   public String toString(){
+    return "Account number " + accountNumber + " has a bank balance of: $" + balance;
+   }
+
+
+
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -1,12 +1,50 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class BankClient {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String response;
+        String prefName;
         System.out.println("What is your preferred name?");
         String name = scan.nextLine();
-//        // Create a bank account with the given name and the account number 101134
-//        BankAccount account = new BankAccount(name, 101134);
+        BankAccount account = new BankAccount(name, 101134);
+
+        while(true){
+            System.out.println("Hi " + account.getPreferredName() + ", What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)? ");
+            response = scan.nextLine();
+            if(Objects.equals(response, "check balance")){
+                System.out.println("Current Balance: " + account.getBalance());
+            }
+            else if(Objects.equals(response, "deposit")){
+                System.out.println("How much money would you like to deposit?");
+                account.deposit(Integer.parseInt(scan.nextLine()));
+            }
+
+            else if(Objects.equals(response, "withdraw")){
+                System.out.println("How much money would you like to withdraw?");
+                    account.withdraw(Integer.parseInt(scan.nextLine()));
+
+            }
+            else if(Objects.equals(response, "check account number")){
+                System.out.println("Account Number: " + account.getAccountNumber());
+            }
+            else if(Objects.equals(response, "set preferred name")){
+                System.out.println("What would you like to set your preferred name to? ");
+                prefName = scan.nextLine();
+                account.setPreferredName(prefName);
+                System.out.println("Name successfully changed to: " + prefName);
+            }
+            else if(Objects.equals(response, "print summary")){
+                System.out.println(account);
+            }
+
+
+
+
+
+
+        }
     }
 }
